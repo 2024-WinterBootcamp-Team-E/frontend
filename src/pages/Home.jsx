2bd448324325e9aa2223efc7ceac47dfdba8a741
TaciTa <seoyoung720@gmail.com>
2025-01-08 23:19:29 +0900

    set : Zustand 세팅
    Zustand 확인을 위한 샘플 코드 '@/store' 작성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import useSampleStore from '@/store';
 
 const Home = () => {
+	const count = useSampleStore((state) => state.count);
+	const increment = useSampleStore((state) => state.increment);
+	const decrement = useSampleStore((state) => state.decrement);
+
 	return (
 		<div>
 			<h1>Home Page</h1>
+			<p>Count : {count}</p>
+			<button onClick={increment}>Increment</button>
+			<button onClick={decrement}>Decrement</button>
 		</div>
 	);
 };
