cf2fb5011efe9d3f95de1f8f5df141f622c3c58b
TaciTa <seoyoung720@gmail.com>
2025-01-13 03:13:55 +0900

    feat : Dropdown 구현 및 Home.jsx에 설정 완료
    animation 오류 있음

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSampleStore from '@/store';
 import styled from 'styled-components';
 import Button from '@/components/Button';
+import DropDown from '@/components/DropDown';
+
+const Home = () => {
+	const count = useSampleStore((state) => state.count);
+	const increment = useSampleStore((state) => state.increment);
+	const decrement = useSampleStore((state) => state.decrement);
+	const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+	return (
+		<StyledBackGround>
+			<TestH1>Home Page</TestH1>
+			<TestP>Count : {count}</TestP>
+			<div>
+				<Button onClick={increment} varient='white'>
+					Increment
+				</Button>
+				<Button onClick={decrement} varient='white'>
+					Decrement
+				</Button>
+			</div>
+			<Button onClick={() => setIsDropDownOpen(!isDropDownOpen)} varient='white'>
+				setIsDropDownOpen
+			</Button>
+			<DropDown isDropDownOpen={isDropDownOpen} />
+		</StyledBackGround>
+	);
+};
+
+const StyledBackGround = styled.div`
+	background-color: var(--neutral-50);
+	width: 100%;
+	height: 100%;
+	padding: 4rem;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+`;
 
 const TestH1 = styled.h1`
 	color: var(--primary-main);
 `;
 
 const TestP = styled.p`
-	font-size: 16px;
+	font-size: 1rem;
 `;
-
-const Home = () => {
-	const count = useSampleStore((state) => state.count);
-	const increment = useSampleStore((state) => state.increment);
-	const decrement = useSampleStore((state) => state.decrement);
-
-	return (
-		<div>
-			<TestH1>Home Page</TestH1>
-			<TestP>Count : {count}</TestP>
-			<button onClick={increment}>Increment</button>
-			<Button onClick={decrement}>Decrement</Button>
-		</div>
-	);
-};
 
 export default Home;
