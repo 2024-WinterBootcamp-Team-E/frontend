cc461cc7dae426601429fe1f97e99a1f0e705e1f
KangIn <gangin0218@gmail.com>
2025-01-15 00:38:42 +0900

    feat: header 컴포넌트추가(수정필요)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Button from '@/components/Button';
 import DropDown from '@/components/DropDown';
 import ChatBubble from '@/components/ChatBubble';
 import chatData from '@/mock/chatData';
+import Header from '@/components/Header'
 
 const Home = () => {
 	const count = useSampleStore((state) => state.count);
@@ -15,6 +16,7 @@ const Home = () => {
 	console.log('home', messages);
 	return (
 		<StyledBackGround>
+			<Header/>
 			<TestH1>Home Page</TestH1>
 			<TestP>Count : {count}</TestP>
 			<div>
@@ -40,7 +42,10 @@ const StyledBackGround = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
-	padding: 4rem;
+	// width: 100%;
+	width: 100vw;
+	overflow:hidden;
+	position:relative;
 `;
 
 const TestH1 = styled.h1`
